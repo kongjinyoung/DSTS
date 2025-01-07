3420fb268e2361093fa69ea50a5b0c62c618d7a9
kongjinyoung <klmonj14@gmail.com>
2025-01-07 11:20:28 +0900

    Initialize

---
+++ ApplicationDSTS/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationDSTS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // 페이지 이동
+        private void btn_monitoring_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_history_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // 편의 기능
+        private void InfoBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ ApplicationDSTS/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDSTS.ViewModels
+{
+    public class HistoryViewModel : ViewModelBase
+    {
+    }
+}
